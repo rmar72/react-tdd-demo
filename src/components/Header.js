51852d8c28a2e3caca65e9d9950082c0947573c1
rsol7 <rsolm72@gmail.com>
2018-12-21 01:16:52 -0700

    a few styles and image for readme display

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './Header.css'
 
 class Header extends Component{
     render(){
         return (
             <div>
-                <img src="images/logo.png" alt="wired-coffee brand logo" />
+                <section>
+                    <img src="images/logo.png" alt="wired-coffee brand logo" />
+                </section>
             </div>
         );
     }
